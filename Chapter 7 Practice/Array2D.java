02324490d1555c296b00d755ffc346425b6ac695
gcschmit <gcschmit@gmail.com>
2014-11-24 10:50:59 -0600

    Finished Array2D

--- Chapter 7 Practice/Array2D.java
+++ Chapter 7 Practice/Array2D.java
@@ -31,10 +31,31 @@ public class Array2D
     
     public String getRow( int row )
     {
+        String str = "";
+        
+//         for( int col = 0; col < table[row].length; col++ )
+//         {
+//             str += table[row][col] + "\t";
+//         }
+
+        for( int val : table[row] )
+        {
+            str += val + "\t";
+        }
+        
+        return str;
     }
     
     public String getCol( int col )
     {
+        String str = "";
+        
+        for( int row = 0; row < table.length; row++ )
+        {
+            str += table[row][col] + "\t";
+        }
+        
+        return str;
     }
     
     public static void main(String[] args)
